5512fa59270ec0885046105f8968af0afc60f4d6
Arjun Shenoy <arjunshenoy23@gmail.com>
2024-07-23 19:20:45 +0530

    Full App

--- item/views.py
+++ item/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 # Create your views here.
 
-from .models import Item
-from .forms import NewItemForm
+from .models import Item, Category
+from .forms import NewItemForm, EditItemForm
+
+def items(request):
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html',{
+        'items': items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id)
+                })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -37,3 +53,25 @@ def delete(request, pk):
     item.delete()
 
     return redirect("dashboard:index")
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form':form,
+        'title': 'New item',
+    })
